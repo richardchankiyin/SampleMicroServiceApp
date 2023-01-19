cd9f27e7e3e493a2b526191d631f0ca22aa1b50b
Richard Chan <richardchankiyin@gmail.com>
2023-01-19 12:15:00 +0800

    add TODO

--- authenticationservice/src/main/java/com/richard/authenticationservice/AuthenticationServiceController.java
+++ authenticationservice/src/main/java/com/richard/authenticationservice/AuthenticationServiceController.java
@@ -5,20 +5,21 @@ package com.richard.authenticationservice;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class AuthenticationServiceController {
 	
+	//TODO to be implemented
 	@PostMapping("/createAccount")
     public String createAccount(@RequestBody String accountDetail) {
-        return "To be implemented";
+        return "To be implemented. Request: " + accountDetail;
     }
 	
+	//TODO to be implemented
 	@PostMapping("/login")
     public String login(@RequestBody String credential) {
-        return "To be implemented";
+        return "To be implemented. Request: " + credential;
     }
 }
